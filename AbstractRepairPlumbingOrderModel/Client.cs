8e31a7d2073f3aadb3b6151b7b084f3bced1ea8c
Akoraito Kurai <hanshuey@mail.ru>
2019-05-06 01:05:56 +0400

    delete comment

--- AbstractRepairPlumbingOrderModel/Client.cs
+++ AbstractRepairPlumbingOrderModel/Client.cs
@@ -16,7 +16,5 @@ namespace AbstractRepairOrderModel
         public int Id { get; set; }
         [Required]
         public string ClientFIO { get; set; }
-        //[ForeignKey("ClientId")]
-        //public virtual List<Order> Orders { get; set; }
     }
 }
