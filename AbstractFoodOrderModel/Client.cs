2164628af1f3eb51d72d7f1588ddf50d27175c4c
Akoraito Kurai <hanshuey@mail.ru>
2019-05-06 07:16:56 +0400

    fix laba1 branch

---
+++ AbstractFoodOrderModel/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractRepairOrderModel
+{
+    /// <summary>
+    /// Клиент доставки
+    /// </summary>
+    public class Client
+    {
+        public int Id { get; set; }
+        public string ClientFIO { get; set; }
+    }
+}
